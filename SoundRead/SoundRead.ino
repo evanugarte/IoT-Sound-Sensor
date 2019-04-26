6cfa58779145e9ff658b5903f1175fc5f586c37f
Evan Ugarte <evanuxd@gmail.com>
2019-04-25 18:34:10 -0700

    Added Average Sound Value Publishing
    Now, our ESP32s will take an average of the sound data instead of
    publishing whatever the instantaneous value is for the sound sensor
    every minute.

--- SoundRead/SoundRead.ino
+++ SoundRead/SoundRead.ino
@@ -20,7 +20,15 @@ void setup() {
   setupCloudIoT();
 }
 
-unsigned long lastMillis = 0;
+/**
+ * The global variables correspond to: 
+ * start: the start time that sound was read,
+ * sound_avg: the average reading of the sound overtime,
+ * count: the number of times sound was read before publishing a message
+ */
+unsigned long start = 0;
+double sound_avg = 0;
+int count = 0;
 
 void loop() {
     mqttClient->loop();
@@ -30,15 +38,36 @@ void loop() {
     connect();
   }
 
+  /* With Each loop, add the reading to the average and increment count*/
+  sound_avg += (double)analogRead(A0);
+  count++;
+  
+  /**
+   * This if statement checks if a minute has elasped, and if the condition
+   * is true, we send a message to the Google PubSub topic.
+   */
+  if (millis() - start > 60000) {
+    /* Update the start value to the current time */
+    start = millis();
 
-  if (millis() - lastMillis > 60000) {
-    lastMillis = millis();
+    /* divide the total sound reading by the average */
+    sound_avg /= (double)count;
+    
+    /**
+     * This is where we format the message, the location 
+     * corresponds to a corner in the room ("NE", "NW", "SE", "SW")
+    */
+    String location = "NE"
+    String payload = location + ": " + String(sound_avg);
 
-    int val = analogRead(A0);
+    // Uncomment the lines below for debugging
+    // Serial.println("Time's up! The average was: " + payload + "\n");
+    // Serial.println(String(count));
 
-    String payload = String(val);
-    Serial.println(payload + "\n");
-    //publishTelemetry(mqttClient, "/sensors", getDefaultSensor());
+    /* Reset the count and sound_avg to 0 */ 
+    count = 0;
+    sound_avg = 0;
+    /* publish the message to GCP. */
     publishTelemetry(payload);
   }
 }
