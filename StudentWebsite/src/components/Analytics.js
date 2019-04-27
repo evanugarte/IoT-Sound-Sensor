553437844b64ad2802f004b7c5bc9c193fac6dbd
Evan Ugarte <evanuxd@gmail.com>
2019-04-27 15:22:09 -0700

    Added simple website components

---
+++ StudentWebsite/src/components/Analytics.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "reactstrap";
+import { getSoundData } from "../firebase/SoundDataActions";
+
+export default class Analytics extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  handlePress = () => {
+    getSoundData("Thursday", "15");
+    console.log("GO");
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.handlePress}>Analytics!</Button>
+      </div>
+    );
+  }
+}
+
--- StudentWebsite/src/components/CurrentSound.js
+++ StudentWebsite/src/components/CurrentSound.js
@@ -1,3 +1,12 @@
 import React from "react";
 
+export default class CurrentSound extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Current Readings</p>
+      </div>
+    );
+  }
+}
 
