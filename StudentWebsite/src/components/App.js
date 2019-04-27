70b9f355d818d0534bd7f88e9cbe791eb4f0ae1b
Evan Ugarte <evanuxd@gmail.com>
2019-04-27 10:17:55 -0700

    Made Website Shell
    Easy website has been made, next is to add firebase functions

---
+++ StudentWebsite/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+function App() {
+  return (
+    <Container>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+    </Container>
+  );
+}
+
+export default App;
