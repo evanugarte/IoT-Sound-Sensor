97ac7e7c676259ebdec4437d4164465da792edb1
Evan Ugarte <evanuxd@gmail.com>
2019-04-27 20:55:27 -0700

    Added Listener for New Data Added

--- StudentWebsite/src/firebase/SoundDataActions.js
+++ StudentWebsite/src/firebase/SoundDataActions.js
@@ -1,14 +1,14 @@
 import * as firebase from "firebase";
 import config from "../config/fbConfig";
+import { getDay, getHour } from "../helpers/TimeConvert";
 
 firebase.initializeApp(config);
 let database = firebase.firestore();
 
-
-export const getSoundData = (day, hour) => {
+export const getSoundData = () => {
+  console.log("getting sound from ", getDay(), getHour());
   let searchResults = [];
-  console.log("walking in ny");
-  database.collection("sound-data").doc(day).collection(hour)
+  database.collection("sound-data").doc(getDay()).collection(getHour())
     .get()
     .then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
@@ -17,6 +17,20 @@ export const getSoundData = (day, hour) => {
         searchResults.push(curr);
       });
     });
+  return searchResults;
 };
 
+export const listenForData = (callback) => {
+  
+  database.collection("sound-data").doc(getDay()).collection(getHour())
+    .onSnapshot(function(snapshot) {
+      snapshot.docChanges().forEach(function(change) {
+        if(change.type === "added")
+        {
+          console.log("sum happened mane");
+          callback();
+        }
+      });
+    });
+};
 
