0362433b61cb938ba7af62044074f506b53f6606
Evan Ugarte <evanuxd@gmail.com>
2019-04-29 15:49:23 -0700

    Added Beginning of Module to Show Analytics
    I added a dropdown to specify the day, the next moves are to send this
    change to our firebaseActions to get the day and hour for each time.

--- StudentWebsite/src/components/Analytics.js
+++ StudentWebsite/src/components/Analytics.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Button } from "reactstrap";
+import { 
+  Button, 
+  ButtonDropdown, 
+  DropdownToggle, 
+  DropdownItem,
+  DropdownMenu
+} from "reactstrap";
 import { getSoundData } from "../firebase/SoundDataActions";
 
 export default class Analytics extends React.Component {
@@ -7,18 +13,68 @@ export default class Analytics extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+      dropdownOpen: false,
+      days: [
+        {index: 0, name: "Sunday"},
+        {index: 1, name: "Monday"},
+        {index: 2, name: "Tuesday"},
+        {index: 3, name: "Wednesday"},
+        {index: 4, name: "Thursday"},
+        {index: 5, name: "Friday"},
+        {index: 6, name: "Saturday"}
+      ],
+      selectedDay: "",
+      selectedDayIndex: -1
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
   }
 
   handlePress = () => {
-    getSoundData();
-    console.log("GO");
+    getSoundData(this.getSoundValues);
+  }
+
+  handleChange = (e) => {
+    console.log(e);
+    this.setState({
+      selectedDay: e.target.value,
+      index: e.target.id
+    });   
+  }
+
+  getSoundValues = (arr) => {
+    arr.map(el => console.log(el));
   }
 
   render() {
     return (
       <div>
         <Button onClick={this.handlePress}>Analytics!</Button>
+        <ButtonDropdown 
+          isOpen={this.state.dropdownOpen} 
+          toggle={this.toggle}>
+          <DropdownToggle caret>
+            {this.state.selectedDay === "" ? "Select Day..." : this.state.selectedDay}
+          </DropdownToggle>
+          <DropdownMenu>
+            {this.state.days.map((day) => {
+              return (
+                <DropdownItem
+                  key={day.index}
+                  id={day.index}
+                  value={day.name}
+                  onClick={this.handleChange}>
+                  {day.name}
+                </DropdownItem>
+              );
+            })}
+          </DropdownMenu>
+        </ButtonDropdown>
       </div>
     );
   }
