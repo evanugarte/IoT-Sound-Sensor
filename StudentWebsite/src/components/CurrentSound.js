e96a6e4c7152b6d801523a99ab862a942fd5fc9e
Evan Ugarte <evanuxd@gmail.com>
2019-04-29 15:47:27 -0700

    Added Component to Show Sound by Minute
    This component will return the sound for every minute, and if no reading
    exists from the hour, the component will tell the user to wait at least
    for one minute for a reading.

--- StudentWebsite/src/components/CurrentSound.js
+++ StudentWebsite/src/components/CurrentSound.js
@@ -1,19 +1,49 @@
 import React from "react";
+import { listenForData } from "../firebase/SoundDataActions";
 
 export default class CurrentSound extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+      pub: 0,
+      currentReading: null,
+      justLoaded: true
+    };
+    this.firstListen = false;
   }
 
   componentDidMount() {
-    
+    listenForData(this.updateStuff);
+  }
+
+  handleFirstSoundRead = (sound) => {
+    let sum = 0;
+    if(sound.length !== 0) {
+      sound.map((item) => sum += parseFloat(item.sound));
+      sum /= sound.length;
+      this.setState({
+        justLoaded: false
+      }, () => setTimeout(() => {}, 1000));
+    }
+    this.updateStuff(sum);
+    listenForData(this.updateStuff, false);
+  }
+
+  updateStuff = (data) => {
+    this.setState({
+      pub: this.state.pub + 1,
+      currentReading: data.sound
+    });
   }
 
   render() {
+    const {currentReading} = this.state;
     return (
       <div>
-        <p>Current Readings</p>
+        <p>Current Readings: { currentReading ? 
+          currentReading : 
+          "Please wait one minute while we fetch the most recent reading"}</p>          
+        <p>Readings within the hour: {this.state.pub}</p>
       </div>
     );
   }
